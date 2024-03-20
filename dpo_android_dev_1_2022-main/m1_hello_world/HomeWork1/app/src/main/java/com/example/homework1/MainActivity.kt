d3092d65fdc526c2001f3cf648d49a850c9168a0
Verenich Tatiana <verenik91@gmail.com>
2024-03-20 12:15:25 +0300

    доработки по коду

--- dpo_android_dev_1_2022-main/m1_hello_world/HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
+++ dpo_android_dev_1_2022-main/m1_hello_world/HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
@@ -23,54 +23,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.reduceView.isEnabled = false //состояние пустого
 
-
         binding.reduceView.setOnClickListener {
             binding.helloView.setTextColor(Color.BLUE)
             binding.addView.isEnabled = true
             binding.restartView.visibility = View.INVISIBLE
-            if (cnt > 0) {  //перенести в метод reduceCounter
-                cnt--
-                binding.counterView.text = cnt.toString()
-                cntPlaces++
-                if (cnt == 0) {
-                    binding.reduceView.isEnabled = false  //состояние пустого
-                    binding.helloView.setTextColor(Color.GREEN)
-                    binding.helloView.text = getString(R.string.empty)
-                } else {
-                    binding.helloView.text = getString(R.string.noEmpty) + " "+ cntPlaces
-                }
-            }
-            else binding.reduceView.isEnabled = true
-
+            cnt-- // reduceCounter
+            binding.counterView.text = cnt.toString()
+            cntPlaces++
+            binding.helloView.text = getString(R.string.noEmpty) + " " + cntPlaces
+            if (cnt <= 0) {
+                binding.reduceView.isEnabled = false  //состояние пустого
+                binding.helloView.setTextColor(Color.GREEN)
+                binding.helloView.text = getString(R.string.empty)
+            } else binding.reduceView.isEnabled = true
         }
 
-        binding.addView.setOnClickListener {
+
+        binding.addView.setOnClickListener{
             binding.reduceView.isEnabled = true
             binding.helloView.setTextColor(Color.BLUE)
-            if (cnt < COUNT_PASSENGER) {
-                cnt++   //перенести в addCounter
-                binding.counterView.text = cnt.toString()
-                cntPlaces--
-                if (cntPlaces == 0) {   //перенести в fullCounter состояние полного
-                    binding.addView.isEnabled = false
-                    binding.helloView.text = getString(R.string.full)
-                    binding.helloView.setTextColor(Color.RED)
-                    binding.restartView.visibility = View.VISIBLE
-                    binding.restartView.setOnClickListener {
-                        cnt = 0  //состояние пустого
-                        cntPlaces = COUNT_PASSENGER
-                        binding.helloView.setTextColor(Color.GREEN)
-                        binding.restartView.visibility = View.INVISIBLE  //перенести в emptyCounter, добавить цвет
-                        binding.helloView.text = getString(R.string.empty)
-                        binding.counterView.text = cnt.toString()
-                        binding.addView.isEnabled = true
-                        binding.reduceView.isEnabled = false
-                    }
-                } else {
-                    binding.helloView.text = getString(R.string.noEmpty) + " " + cntPlaces
-                }
+            cnt++
+            binding.counterView.text = cnt.toString()
+            cntPlaces--
+            binding.helloView.text = getString(R.string.noEmpty) + " " + cntPlaces
+            if (cntPlaces <= 0) {   //fullCounter
+                binding.helloView.text = getString(R.string.full)
+                binding.helloView.setTextColor(Color.RED)
+                binding.restartView.visibility = View.VISIBLE
+                binding.addView.isEnabled = false
             }
-            else binding.addView.isEnabled = false
+        }
+
+        binding.restartView.setOnClickListener{
+            cnt = 0  //emptyCounter
+            cntPlaces = COUNT_PASSENGER
+            binding.helloView.setTextColor(Color.GREEN)
+            binding.restartView.visibility = View.INVISIBLE
+            binding.helloView.text = getString(R.string.empty)
+            binding.counterView.text = cnt.toString()
+            binding.reduceView.isEnabled = false
         }
     }
 }
